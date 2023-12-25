9aa83bd0acdc8ece0b2e5f807b29e7d760eb9980
Alexander Mishchenko <129165793+Alxmis@users.noreply.github.com>
2023-12-25 09:49:25 +0300

    done

--- states/states.py
+++ states/states.py
@@ -4,11 +4,12 @@ class DataState(StatesGroup):
     choosing_sex = State()
     choosing_age = State()
     choosing_name = State()
-    fin_main_info = State()
     choosing_interest = State()
+    fin_main_info = State()
     change_name = State()
     change_age = State()
     change_sex = State()
+    change_interest = State()
 
 class MainState(StatesGroup):
     in_main = State()
@@ -17,3 +18,4 @@ class MainState(StatesGroup):
     ch_age = State()
     ch_name = State()
     ch_sex = State()
+    ch_interest = State()
